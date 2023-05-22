0eb7179e9586de937d5502c561b8db2a42ec72f8
zeroluffs <zeroluffs@gmail.com>
2023-05-22 15:14:59 -0500

    copies URL to clipboard

---
+++ src/app/CopyUrl.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { useState } from 'react'
+import { classNames } from '../../utils/JoinClassnames'
+interface CopyUrlProps {
+  url: string
+}
+
+export function CopyUrl({ url }: CopyUrlProps) {
+  const [copyState, setCopyState] = useState(false)
+  return (
+    <div className="flex flex-col gap-1">
+      <div className="flex flex-row">
+        <div className="w-96 h-12 bg-slate-100 shadow-sm border rounded-lg hove border-sky-950 flex items-center px-3">
+          {url}
+        </div>
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText(url)
+            setCopyState(true)
+          }}
+          disabled={copyState}
+          className={classNames(
+            copyState ? 'bg-green-600 ' : 'bg-sky-950 hover:bg-sky-700',
+            'transition duration-200 text-white rounded-lg  shadow-sm h-12 w-44 ml-2'
+          )}>
+          {copyState ? 'Copied!' : 'Copy To Clipboard'}
+        </button>
+      </div>
+    </div>
+  )
+}
--- src/app/InputField.tsx
+++ src/app/InputField.tsx
@@ -3,10 +3,13 @@
 import { useState } from 'react'
 import { classNames } from '../../utils/JoinClassnames'
 import { useQuery } from 'react-query'
+import { LoadingRequest } from './LoadingRequest'
+import { CopyUrl } from './CopyUrl'
 
 export function InputField() {
   const [url, setUrl] = useState('')
   const [isValid, setIsValid] = useState(false)
+  const [generateState, setGenerateState] = useState(false)
 
   const fetchUrl = async () => {
     const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
@@ -15,6 +18,10 @@ export function InputField() {
   }
 
   const { data, isLoading, error, refetch } = useQuery('getUrl', fetchUrl, {
+    onSuccess: (data) => {
+      setUrl(data.short_link)
+      setGenerateState(true)
+    },
     enabled: false,
   })
   const handleClick = () => {
@@ -25,32 +32,40 @@ export function InputField() {
       setIsValid(false)
     }
   }
-  return (
-    <div className="flex flex-col gap-1">
-      <p
-        className={classNames(
-          isValid ? 'hidden' : 'block',
-          'text-sm transition duration-100 text-red-500'
-        )}>
-        Input a correct URL
-      </p>
-      <div className="flex flex-row">
-        <input
-          type="text"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          className="w-96 h-12 shadow-sm border rounded-lg hove border-sky-950 p-4"
-          placeholder="Type Your URL"></input>
-        <button
-          onClick={handleClick}
-          className="bg-sky-950 hover:bg-sky-700 transition duration-200 text-white rounded-lg  shadow-sm h-12 w-40 ml-2">
-          Shorten URL
-        </button>
-      </div>
-    </div>
-  )
-}
 
+  const GenerateState = () => {
+    if (!generateState) {
+      return (
+        <div className="flex flex-col gap-1">
+          <p
+            className={classNames(
+              isValid ? 'hidden' : 'block',
+              'text-sm transition duration-100 text-red-500'
+            )}>
+            Input a correct URL
+          </p>
+          <div className="flex flex-row">
+            <input
+              type="text"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+              className="w-96 h-12 shadow-sm border rounded-lg hove border-sky-950 p-4"
+              placeholder="Type Your URL"></input>
+            <button
+              onClick={handleClick}
+              className="bg-sky-950 hover:bg-sky-700 transition duration-200 text-white rounded-lg  shadow-sm h-12 w-40 ml-2">
+              Shorten URL
+            </button>
+          </div>
+        </div>
+      )
+    } else {
+      return <CopyUrl url={url} />
+    }
+  }
+
+  return isLoading ? <LoadingRequest /> : <GenerateState />
+}
 function isValidUrl(url: string): boolean {
   const urlPattern = /^(?:https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i
   return urlPattern.test(url)
